9c6ef422827105daa05c14ca589bfd7058c59ffd
abdumannab-usmanov <usmanovabdumannab2307@gmail.com>
2022-02-21 23:01:48 +0500

    Initial commit

---
+++ css/main.js
@@ -0,0 +1,9 @@
+var money = (9647716)
+var a = money
+var b = prompt ('how much money do you have')
+
+
+if (money <= b
+    ){
+        console.log('Have a nice journey');
+    }else{console.log('Sorry you have to earn money');}
