7540d5c72d84df3f5ede90e61dc3a060e9c4244c
abdumannab-usmanov <usmanovabdumannab2307@gmail.com>
2022-02-23 23:15:13 +0500

    Add small change

--- css/main.js
+++
@@ -1,9 +0,0 @@
-var money = (9647716)
-var a = money
-var b = prompt ('How much money do you have ?')
-
-
-if (money <= b
-    ){
-        console.log('Have a nice journey');
-    }else{console.log('Sorry you have to earn money');}
